9292da0e6bef77bb3abe44e3ec6bfd1c39e2f2c5
marius <marius@gmail.com>
2025-05-23 11:16:36 +0200

    init

---
+++ 3.1.5.py
@@ -0,0 +1,19 @@
+color = "blau"
+size = "groß"
+doors = 3
+engine = "Diesel"
+car_type = "LKW"
+
+tires = "summer"
+windows = "dunkel"
+turbo = True
+deluxe = True
+race_seats = True
+
+print(color, size, doors, engine, car_type, tires, windows, turbo, deluxe, race_seats)
+
+x = color
+color = size
+size = x
+
+print(color, size, doors, engine, car_type, tires, windows, turbo, deluxe, race_seats)
