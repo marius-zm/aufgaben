f95bcc6cd55a214e6f5794756b9491e5085545ba
marius <marius@gmail.com>
2025-05-27 13:12:23 +0200

    3.2.6

---
+++ 3.2.6.py
@@ -0,0 +1,14 @@
+obst = ["Apfel", "Banane", "Traube", "Birne"]
+noten = [1, 2, -3, 2, 4]
+mix = ["Schrott", 10, "Buch", 2.45]
+
+print(obst[0])
+print(noten[0])
+print(type(obst))
+print(type(obst[0]))
+
+print(type(obst[-1]))
+print(type(obst[0]))
+
+obst[1] = "Kirsche"
+print(obst)
