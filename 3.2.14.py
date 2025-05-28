470992e323fe6ab8a65eb0b2c4fa2c1c3a200ce2
marius <marius@gmail.com>
2025-05-28 15:02:44 +0200

    3.2.14

--- 3.2.14.py
+++ 3.2.14.py
@@ -12,10 +12,24 @@ class Role (Person):
         super().__init__(name, gender, height)
     
         self.role = role
+        
+        if self.role == "Priester":
+            self.attributes = {
+                "health": 80,
+                "armour": 20,
+                "intelligence": 15,
+                "wisdom": 18,
+                "strength": 9,
+                "dexterity": 12,
+            }
 
     def sub_class(self):
         print(f"Ich bin {self.name} und {self.role}.")
 
+    def what_are_my_stats(self):
+        print(self.attributes)
+
 marius = Role("Marius", "male", 1.80, "Priester")
 marius.greet()
 marius.sub_class()
+marius.what_are_my_stats()
