bc5aac63c2f884ecdd82c6c94e56f6606d1cbcb1
marius <marius@gmail.com>
2025-05-30 11:23:56 +0200

    3.2.15

--- 3.2.15.py
+++ 3.2.15.py
@@ -2,16 +2,23 @@ class BankAccount:
     def __init__(self, owner) -> None:
         self.owner = owner
         self.balance = 0
-        self.limit = 100
+        self.limit = -100
 
     def pay_in(self, amount):
+        if amount <= 0:
+            print("Amount must be greater than 0.")
+            return
         self.balance += amount
 
     def pay_out(self, amount):
-        if self.balance - self.limit < amount:
-            print("Du hast nicht genug Geld auf dem Konto.")
+        if amount <= 0:
+            print("Amount must be greater than 0.")
+            return
+        if self.balance - amount < self.limit:
+            print("That would exceed your account overdraft limit.")
             return
         self.balance -= amount
+        self.show_balance()
 
     def show_balance(self):
         print(f"{self.balance}")
@@ -20,5 +27,19 @@ class BankAccount:
         print(f"Kontoinhaber: {self.owner} - Kontostand: {self.balance}")
 
     def calc_zinsen(self, zinssatz):
+        if zinssatz <= 0:
+            print("Zinssatz must be higher than 0.")
+            return
         if self.balance > 0:
-            self.balance += self.balance * zinssatz
+            self.balance += self.balance * (zinssatz / 100)
+            print(f"New account balance: {self.balance}")
+        else:
+            print("Accountbalance not positiv.")
+
+account1 = BankAccount("Marius Meyer")
+account1.show_balance()
+account1.pay_in(100)
+account1.show_balance()
+account1.calc_zinsen(10)
+account1.pay_out(200)
+account1.pay_out(20)
