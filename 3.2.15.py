7eee9f1758a0527e6406f64ab4195a720330df4f
marius <marius@gmail.com>
2025-05-30 11:47:09 +0200

    3.2.15

--- 3.2.15.py
+++ 3.2.15.py
@@ -9,6 +9,7 @@ class BankAccount:
             print("Amount must be greater than 0.")
             return
         self.balance += amount
+        print(f"New Account balance: {self.balance}")
 
     def pay_out(self, amount):
         if amount <= 0:
@@ -17,30 +18,32 @@ class BankAccount:
         if self.balance - amount < self.limit:
             print("That would exceed your account overdraft limit.")
             return
+        
         self.balance -= amount
-        self.show_balance()
+        print(f"New Account balance: {self.balance}")
 
     def show_balance(self):
-        print(f"{self.balance}")
+        print(f"Account balance: {self.balance}")
 
     def __str__(self) -> str:
-        print(f"Kontoinhaber: {self.owner} - Kontostand: {self.balance}")
+        return f"Account owner: {self.owner} - Account balance: {self.balance}"
 
-    def calc_zinsen(self, zinssatz):
-        if zinssatz <= 0:
-            print("Zinssatz must be higher than 0.")
+    def calc_interest(self, interest_rate):
+        if interest_rate <= 0:
+            print("Interest rate must be higher than 0.")
             return
         if self.balance > 0:
-            self.balance += self.balance * (zinssatz / 100)
+            self.balance += self.balance * (interest_rate / 100)
             print(f"New account balance: {self.balance}")
         else:
-            print("Accountbalance not positiv.")
+            print("Account balance not positive.")
 
 account1 = BankAccount("Marius Meyer")
 account1.show_balance()
 account1.pay_in(100)
 account1.show_balance()
-account1.calc_zinsen(10)
+account1.calc_interest(10)
 account1.pay_out(200)
 account1.pay_out(20)
-account1.__str__()
+account1.calc_interest(3)
+print(account1)
