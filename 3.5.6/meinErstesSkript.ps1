bbebfa119a8a1cfa5284baae1f40e7a98777f8cc
marius <marius.meyer@schule-zukunftsmotor.org>
2025-06-17 15:35:19 +0200

    3.5.6

---
+++ 3.5.6/meinErstesSkript.ps1
@@ -0,0 +1,14 @@
+$name = "Marius"
+$alter = 32
+$greet = "Hallo, mein Name ist $name"
+$vorstellung = "Ich bin $name und $alter alt."
+
+Write-Host $greet $vorstellung
+
+$hobby = "Kochen"
+
+Write-Host "$vorstellung Mein Hobby ist $hobby"
+
+$date = Get-Date
+
+Write-Host $date
